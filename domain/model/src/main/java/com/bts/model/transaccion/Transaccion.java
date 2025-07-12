38e5c093e8f4ec2e352ac52e3d175d44f33f4027
juanDa-dev373 <teamcracks38@gmail.com>
2025-07-12 14:46:16 -0500

    Se crearon controladores

--- domain/model/src/main/java/com/bts/model/transaccion/Transaccion.java
+++ domain/model/src/main/java/com/bts/model/transaccion/Transaccion.java
@@ -1,4 +1,22 @@
 package com.bts.model.transaccion;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class Transaccion {
+
+    private String transaccionId;
+    private String clienteId;
+    private String fondoId;
+    private String tipo;
+    private BigDecimal monto;
+    private LocalDateTime fecha;
+
 }
--- domain/usecase/src/main/java/com/bts/usecase/fondos/FondosUseCase.java
+++ domain/usecase/src/main/java/com/bts/usecase/fondos/FondosUseCase.java
@@ -1,4 +1,17 @@
 package com.bts.usecase.fondos;
 
+import com.bts.model.fondo.Fondo;
+import com.bts.model.fondo.gateway.FondoGateway;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class FondosUseCase {
+
+    private final FondoGateway fondoGateway;
+
+    public Fondo suscribir(Fondo fondo) {
+        return fondoGateway.save(fondo);
+    }
+
+
 }
